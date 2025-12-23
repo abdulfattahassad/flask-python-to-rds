b3fa1756596b07b06ce3fc7e77d4bb72a481954f
mac <mac@gmail.com>
2025-12-23 21:25:06 +0300

    second commit

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://username:password@localhost/dbname'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://Love6000@localhostdb.c18m8comuyav.us-east-1.rds.amazonaws.com/dbname'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
